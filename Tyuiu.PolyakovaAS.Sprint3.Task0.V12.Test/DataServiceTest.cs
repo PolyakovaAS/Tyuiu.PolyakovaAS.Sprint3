80a39a06b9943ab83dcca3d42685f851f52da064
KirillBurdov <Patrikandbooke@gmail.com>
2025-11-15 15:37:32 +0300

    Add project files.

---
+++ Tyuiu.PolyakovaAS.Sprint3.Task0.V12.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.PolyakovaAS.Sprint3.Task0.V12.Lib;
+namespace Tyuiu.PolyakovaAS.Sprint3.Task0.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 13;
+
+            var res = ds.GetMultiplySeries(startValue, stopValue, value);
+
+            Assert.AreEqual(1, res);
+        }
+    }
+}
